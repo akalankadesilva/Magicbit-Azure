6156523ef1ac7075c471e050d2fc7da6920d7cc0
Akalanka <akalankadesilva@gmail.com>
2020-11-21 14:40:53 +1100

    added changes to SAS token format

--- web/generateSAS.js
+++ web/generateSAS.js
@@ -1,18 +1,23 @@
 var utf8=require("utf8");
 var crypto=require("crypto");
-function createSharedAccessToken(uri, saName, saKey) { 
-    if (!uri || !saName || !saKey) { 
-           console.log("Usage: node generateSAS.js <IoT Hub Host name> <Device ID> <Shared Access Key>")
-           process.exit();
-        } 
-    var encoded = encodeURIComponent(uri); 
-    var now = new Date(); 
-    var week = 60*60*24*7;
-    var ttl = Math.round(now.getTime() / 1000) + week;
-    var signature = encoded + '\n' + ttl; 
-    var signatureUTF8 = utf8.encode(signature); 
-    var hash = crypto.createHmac('sha256', saKey).update(signatureUTF8).digest('base64'); 
-    return 'SharedAccessSignature sr=' + encoded + '&sig=' +  
-        encodeURIComponent(hash) + '&se=' + ttl + '&skn=' + saName; 
+function createSASToken(uri,saName,saKey){
+    resourceUri = encodeURIComponent(uri+"/devices/"+saName);
+
+    // Set expiration in seconds
+    var expires = (Date.now() / 1000) + 60*60*24*7;
+    expires = Math.ceil(expires);
+    var toSign = resourceUri + '\n' + expires;
+
+    // Use crypto
+    var hmac = crypto.createHmac('sha256', Buffer.from(saKey, 'base64'));
+    hmac.update(toSign);
+    var base64UriEncoded = encodeURIComponent(hmac.digest('base64'));
+
+    // Construct authorization string
+    var token = "SharedAccessSignature sr=" + resourceUri + "&sig="
+    + base64UriEncoded + "&se=" + expires;
+   
+    return token;
 }
-console.log(createSharedAccessToken(process.argv[2],process.argv[3],process.argv[4]));
+console.log(createSASToken(process.argv[2],process.argv[3],process.argv[4]));
+
